9d76ce9fabf03af1e610406e742da9031139297d
Rafael Ribeiro <105018495+rafaelrlc@users.noreply.github.com>
2023-05-25 22:05:15 -0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import { TerminalContextProvider } from "react-terminal";
-ReactDOM.createRoot(document.getElementById("root")).render(
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <TerminalContextProvider>
-      <App />
-    </TerminalContextProvider>
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
