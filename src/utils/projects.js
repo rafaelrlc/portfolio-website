fc25390714bbce4eb2bf92104d80bfb534b658e5
Rafael Ribeiro <105018495+rafaelrlc@users.noreply.github.com>
2023-12-15 13:19:49 -0300

    update project

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -17,8 +17,18 @@ export const projects = [
       "A website to help the communication between the patient. This fullstack project, the frontend was made in Next.js and Tailwind and the backend in Express and MongoDB and deployed with Railwind.",
     url: "https://github.com/rafaelrlc/Psico-Daily",
   },
+
   {
     id: 3,
+    image:
+      "https://cdn.discordapp.com/attachments/714891795129171983/1185254676900352040/Screenshot_2023-12-15_at_13.15.56.png?ex=658ef16f&is=657c7c6f&hm=b9754f976554993ff39b7c51a6c7819568cfbc37dca513f3c2edeaaaa4be1750&",
+    title: "NamelessBlog.io",
+    description:
+      "A full-stack blog app: the front-end is being developed in React with Next.js 13, while the back-end is being built using Spring Boot (currently in progress)",
+    url: "https://github.com/rafaelrlc/namelessBlog.io",
+  },
+  {
+    id: 4,
     image:
       "https://media.discordapp.net/attachments/714891795129171983/1092821396163403836/image.png?width=1080&height=493",
     title: "Tom Nook Market",
@@ -28,7 +38,7 @@ export const projects = [
   },
 
   {
-    id: 4,
+    id: 5,
     image:
       "https://media.discordapp.net/attachments/886688344363388958/1110224097813733376/image.png?width=1126&height=548",
     title: "Portfolio Website",
@@ -37,21 +47,12 @@ export const projects = [
     url: "https://github.com/rafaelrlc/portfolio-website",
   },
   {
-    id: 5,
+    id: 6,
     title: "Andromeda",
     image:
       "https://upload.wikimedia.org/wikipedia/commons/c/c2/M31_09-01-2011_%28cropped%29.jpg",
     description:
       "A simple anime listing website using a external API, in this project I learned how to do pagination.",
     url: "https://github.com/rafaelrlc/andromeda-anime-list-website",
-  },
-
-  {
-    id: 6,
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/c/c2/M31_09-01-2011_%28cropped%29.jpg",
-    title: "Look-At",
-    description: "A full-stack social media app (currently working on).",
-    url: "https://github.com/rafaelrlc",
   },
 ];
